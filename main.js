bd6b8d669ca58129a63157ef53e04c5d707ab482
JR Powell <jrpowell@FSPFVFZ929ML414.local>
2020-04-03 11:43:27 -0400

    Added Javascript to read hashtag and return one embedded tweet.

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
-$("body").on("click", function (e) {
+$(".calendar").on("click", function (e) {
 	e.preventDefault();
 
-	this_button = e.target.id
+	let date_clicked = ""
+	let hashtag_clicked = ""
 
-	document.getElementById("testoutput").innerHTML = this_button;
+	date_clicked = e.target.id
 
+	document.getElementById("dateoutput").innerHTML = date_clicked;
+
+
+	$(".hashtags").on("click", function (e) {
+		e.preventDefault();
+
+		hashtag_clicked = e.target.id
+
+		document.getElementById("hashtagoutput").innerHTML = hashtag_clicked;
+		if (date_clicked === "feb15" & hashtag_clicked === "#quarantena") {
+			$(".twitter-tweet").removeClass('inactive');
+		} else {
+			$(".twitter-tweet").addClass('inactive');
+		}
+	});
 });
