80a011feb834f954ac1fb37ddbb9d30aea078cbf
JR Powell <jrpowell@FSPFVFZ929ML414.local>
2020-04-13 22:11:28 -0400

    fixed stuff, lol

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
+let hashtag = " "
+let date_clicked = " "
+
 $(".calendar").on("click", function (e) { // Takes an date and hashtag input.
 	e.preventDefault();
 
-	let date_clicked = ""
 	date_clicked = e.target.id
 	document.getElementById("dateoutput").innerHTML = date_clicked;
+
+	if (hashtag != " " & date_clicked != " ") {
+		console.log("Publishing corresponding tweets!")
+	};
 });
 
 $(".filter").on("change", function (e) { // Takes an date and hashtag input.
 	hashtag = $(".filter").val();
 	document.getElementById("hashtagoutput").innerHTML = hashtag
+
+	if (hashtag != " " & date_clicked != " ") {
+		console.log("Publishing corresponding tweets!")
+	};
 });
